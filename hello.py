f972a93b313dacda2ff08d321aed9fa19b3a5726
Anoop Macharla <anoopm@MacBook-Pro-3.fios-router.home>
2018-03-19 16:58:25 -0400

    config for Travis test done

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.rout("/")
+def hello():
+    return "Hello in Flask, Python!"
+
+if __name__ == "__main__":
+    app.run()
